de4d9ee26eaa5fb5900144d06560d9f17710caff
Gssssssk <Geeeees@users.noreply.github.com>
2015-11-29 14:11:05 -0300

    Category

---
+++ gsk/Category.java
@@ -0,0 +1,15 @@
+package gsk.onetcore;
+
+public enum Category {
+    DEFAULT,
+    VIP,
+    TMOD,
+    MOD,
+    DIRECTOR,
+    ADMIN,
+    OP;
+    
+
+    private Category(String string2, int n2) {
+    }
+}
